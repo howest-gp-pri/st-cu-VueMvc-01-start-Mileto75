0ddda665996a37d49653910ab3724c019c781619
Miledi75 <mileto.di.marco@howest.be>
2023-11-21 14:04:29 +0100

    Add Mvc project

---
+++ Cu.VueMvc.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cu.VueMvc.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

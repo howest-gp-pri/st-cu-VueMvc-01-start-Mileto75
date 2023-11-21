6e69ccc6f8e0a8345be0b236783195388d7dfd02
Miledi75 <mileto.di.marco@howest.be>
2023-11-21 15:16:26 +0100

    Add vue get categories

--- Cu.VueMvc.Web/wwwroot/js/app.js
+++ Cu.VueMvc.Web/wwwroot/js/app.js
@@ -1,6 +1,33 @@
 ﻿let app = new Vue({
     el: "#app",
     data: {
-        pageTitle: "Vue in Mvc"
-    }
+        pageTitle: "Vue in Mvc",
+        categoriesVisible: false,
+        productsVisible: false,
+        baseUrl: "https://localhost:44326/api",
+        categories: [],
+        hasError: false,
+        errorMessage: "",
+    },
+    methods: {
+        showCategories: async function () {
+            this.hasError = false;
+            this.productsVisible = false;
+            this.categoriesVisible = true;
+            //get the categories from the api
+            this.categories = await axios.get(`${this.baseUrl}/categories`)
+                .then(response => response.data.items)
+                .catch(error => {
+                    if (error.response.status == 404) {
+                        this.errorMessage = "endpoint not found!";
+                    }
+                    this.hasError = true;
+                });
+        },
+        showProducts: function () {
+            this.categoriesVisible = false;
+            this.productsVisible = true;
+            //get the products from the api
+        },
+    },
 });
